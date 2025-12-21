e191afdbbb9a7983c13b0d079ccbf055e60898c3
Jack Madsen <jackcmadsen@icloud.com>
2025-12-21 09:42:14 -0500

    I hope nobody is trying to use this right now

--- api/api.js
+++ api/api.js
@@ -2,7 +2,7 @@ import StudentVue from "studentvue.js";
 import crypto from "crypto";
 import cookie from "cookie";
 
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
+const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY || "21643a2fa460d306a3c3d2598c3ea0fd";
 
 function encrypt(text) {
   const iv = crypto.randomBytes(16);
